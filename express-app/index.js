d9b739429c165389544e9ad13e6148bf249e03d3
Ronit Mishra <ronnie94official@gmail.com>
2018-09-29 18:25:58 +0530

    added routes

--- express-app/index.js
+++ express-app/index.js
@@ -4,9 +4,23 @@ import dataFile from './data/data.json';
 
 const app = express();
 
+
+
 app.set('port', process.env.PORT || 3000);
 
+
+
+/*Base Route*/
 app.get('/', (request,response) => {
+
+	response.send(`
+		<h1>Welcome Peeps!</h1>
+		`);
+});
+
+
+/*Speakers Route*/
+app.get('/speakers', (request,response) => {
 
 	var info = '';
 	dataFile.speakers.forEach((item) => {
@@ -18,13 +32,28 @@ app.get('/', (request,response) => {
 		`;
 	});
 
-
-
 	response.send(`
-		<h1>Greetings from the Node.js Server</h1>
+		<h1>Greetings from the Ronnies' Node.js Server</h1>
 		${info}
 		`);
 });
+
+
+
+/*Speaker Route*/
+app.get('/speakers/:speakerid', (request,response) => {
+
+	var speaker = dataFile.speakers[request.params.speakerid];
+
+	response.send(`
+		<h1>Greetings from the Ronnies' Node.js Server</h1>
+		<h2>${speaker.title}</h2>
+		<h3> - Presented by: ${speaker.name}</h3>
+		<p>${speaker.summary}</p>
+		`);
+});
+
+
 
 app.listen(app.get('port'), ()=> {
 	console.log("Server initiated on localhost:" + app.get('port'))
